5e60a31037ca3045f46702c6f97571c252e8579d
Allen <378611380@qq.com>
2023-03-08 19:26:33 +0800

    feat: update isObject method logic

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,19 +1,6 @@
 /**
- * @method 获取数据类型
- */
-const getDataType = (data: any): string => {
-  return Object.prototype.toString.call(data).slice(8, -1)
-}
-/**
- * @method 是否是数组
- */
-export const isArray = (data: any): boolean => {
-  return typeof data === 'object' && getDataType(data) === 'Array'
-}
-/**
  * @method 是否是对象
  */
 export const isObject = (data: any) => {
-  // return typeof data === 'object' && data && !Array.isArray(data)
-  return typeof data === 'object' && data !== null && !isArray(data)
+  return typeof data === 'object' && data !== null && !Array.isArray(data)
 }
