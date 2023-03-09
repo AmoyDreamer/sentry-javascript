a67f5d2ae0e41db2afd22d0057164a0c06994f7d
Allen Liu <378611380@qq.com>
2023-03-10 01:21:43 +0800

    feat: update index.d.ts and package.json

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,6 +9,13 @@ import type {
   SentrySDKResponse
 } from './types'
 
+declare module 'sentry-js-sdk' {
+  export function init(options: InitOptions): void
+  export function configureScope(callback: Function): void
+  export function captureMessage(message: string, options?: SentryCaptureOptions): Promise<any> | void
+  export function captureException(err: Error, options?: SentryCaptureOptions): Promise<any> | void
+}
+
 export type {
   InitOptions,
   SentryCaptureOptions,
