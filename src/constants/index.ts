6f80d19425d7aa9d35b10e3ac93e6a562a66493f
Allen Liu <378611380@qq.com>
2023-03-20 00:57:43 +0800

    feat: update constant code comments

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,18 +1,18 @@
-/** http状态码 - Ok */
+/** http status - Ok */
 export const HTTP_STATUS_SUCCESS = 200
-/** http状态码 - Bad Request */
+/** http status - Bad Request */
 export const HTTP_STATUS_BAD_REQUEST = 400
-/** http状态码 - Too Many Requests */
+/** http status - Too Many Requests */
 export const HTTP_STATUS_TOO_MANY_REQUESTS = 429
-/** http状态码 - Internal Server Error */
+/** http status - Internal Server Error */
 export const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500
-/** http状态码 - Payload Too Large */
+/** http status - Payload Too Large */
 export const HTTP_STATUS_PAYLOAY_TOO_LARGE = 413
-/** 禁止重新请求的http状态码集合 */
+/** the collection of http status codes that disabled retry request */
 export const DISABLE_RETRY_HTTP_STATUS_LIST = [HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_TOO_MANY_REQUESTS]
-/** 自定义状态码 - 禁止上传日志 */
+/** custom status code - Disable Log Upload */
 export const CUSTOM_STATUS_DISABLE_UPLOAD_LOG = 10001
-/** http请求 - 最大重试次数 */
+/** http request - Maximum number of retries */
 export const MAX_RETRIES = 1
-/** http请求 - 延迟重试请求时间（单位：毫秒） */
+/** http request - Delayed retry request time (in ms) */
 export const RETRY_DELAY = 1000
