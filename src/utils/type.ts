06cf7639c32f806fc5358a7dabd4fd6a4599bee3
Allen <378611380@qq.com>
2023-03-20 19:42:58 +0800

    feat: update type tool method comments

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,5 +1,5 @@
 /**
- * @method 是否是对象
+ * @method Determine if it is an object data
  */
 export const isObject = (data: any) => {
   return typeof data === 'object' && data !== null && !Array.isArray(data)
